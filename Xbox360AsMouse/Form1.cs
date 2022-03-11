6a344c6a0e661062dac979bd86ccbd7c293f2ab6
SaeedBazargan <31041761+SaeedBazarganZSR@users.noreply.github.com>
2022-03-12 03:27:28 +0330

    The "A, B, Y, X, Start and Back" buttons were launched. The rest of the buttons and joysticks remain.

--- Xbox360AsMouse/Form1.cs
+++ Xbox360AsMouse/Form1.cs
@@ -17,18 +17,15 @@ namespace Xbox360AsMouse
     public partial class Form1 : Form
     {
         private Controller _controller;
-        private IMouseSimulator _mouseSimulator;
+        //private IMouseSimulator _mouseSimulator;
         private System.Threading.Timer _timer;
-        private bool _wasAdown;
-        private bool _wasBdown;
 
         public Form1()
         {
             InitializeComponent();
             _controller = new Controller(UserIndex.One);
-            _mouseSimulator = new InputSimulator().Mouse;
+            //_mouseSimulator = new InputSimulator().Mouse;
             _timer = new System.Threading.Timer(obj => Update());
-            //_timer = new Timer(object => Update);
         }
         public void Start()
         {
@@ -39,42 +36,62 @@ namespace Xbox360AsMouse
             _controller.GetState(out var _state);
             Movement(_state);
             scroll(_state);
-            LeftButton(_state);
-            RightButton(_state);
+            Button_A(_state);
+            Button_B(_state);
+            Button_Y(_state);
+            Button_X(_state);
+            Button_Start(_state);
+            Button_Back(_state);
         }
         private void Movement(State state)
         {
-            var x = state.Gamepad.LeftThumbX / 2_000;
-            var y = state.Gamepad.LeftThumbY / 2_000;
-            _mouseSimulator.MoveMouseBy(x, -y);
+            //var x = state.Gamepad.LeftThumbX / 2_000;
+            //var y = state.Gamepad.LeftThumbY / 2_000;
+            //_mouseSimulator.MoveMouseBy(x, -y);
         }
         private void scroll(State state)
         {
-            var x = state.Gamepad.RightThumbX / 10_000;
-            var y = state.Gamepad.RightThumbY / 10_000;
-            _mouseSimulator.HorizontalScroll(x);
-            _mouseSimulator.VerticalScroll(y);
+            //var x = state.Gamepad.RightThumbX / 10_000;
+            //var y = state.Gamepad.RightThumbY / 10_000;
+            //_mouseSimulator.HorizontalScroll(x);
+            //_mouseSimulator.VerticalScroll(y);
         }
-        private void LeftButton(State state)
+        private void Button_A(State state)
         {
             var isAdown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.A);
-            if (isAdown && !_wasAdown) _mouseSimulator.LeftButtonDown();
-            if (isAdown && _wasAdown) _mouseSimulator.LeftButtonUp();
-            _wasAdown = isAdown;
+            if (isAdown) Console.WriteLine("AAAAA: {0}", isAdown);
         }
-        private void RightButton(State state)
+        private void Button_B(State state)
         {
             var isBdown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.B);
-            if (isBdown && !_wasBdown) _mouseSimulator.RightButtonDown();
-            if (isBdown && _wasBdown) _mouseSimulator.RightButtonUp();
-            _wasAdown = isBdown;
+            if (isBdown) Console.WriteLine("BBBB: {0}", isBdown);
         }
+        private void Button_Y(State state)
+        {
+            var isYdown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Y);
+            if (isYdown) Console.WriteLine("YYYY: {0}", isYdown);
+        }
+        private void Button_X(State state)
+        {
+            var isXdown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.X);
+            if (isXdown) Console.WriteLine("XXXX: {0}", isXdown);
+        }
+        private void Button_Start(State state)
+        {
+            var isStartdown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Start);
+            if (isStartdown) Console.WriteLine("Start: {0}", isStartdown);
+        }
+        private void Button_Back(State state)
+        {
+            var isBackdown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Back);
+            if (isBackdown) Console.WriteLine("Back: {0}", isBackdown);
+        }
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            Update();
             Start();
-            Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxx");
-            Console.ReadLine();
         }
     }
 }
