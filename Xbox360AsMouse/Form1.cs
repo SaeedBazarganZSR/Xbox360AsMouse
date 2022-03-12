5defdc1a053fd617da4b4caf414df9b669c805f7
SaeedBazargan <31041761+SaeedBazarganZSR@users.noreply.github.com>
2022-03-12 03:51:03 +0330

    All buttons except the thumbs were updated.

--- Xbox360AsMouse/Form1.cs
+++ Xbox360AsMouse/Form1.cs
@@ -34,14 +34,22 @@ namespace Xbox360AsMouse
         private void Update()
         {
             _controller.GetState(out var _state);
-            Movement(_state);
-            scroll(_state);
+
+            //Movement(_state);
+            //scroll(_state);
+
             Button_A(_state);
             Button_B(_state);
             Button_Y(_state);
             Button_X(_state);
+
             Button_Start(_state);
             Button_Back(_state);
+
+            Button_DPadDown(_state);
+            Button_DPadUp(_state);
+            Button_DPadLeft(_state);
+            Button_DPadRight(_state);
         }
         private void Movement(State state)
         {
@@ -56,6 +64,7 @@ namespace Xbox360AsMouse
             //_mouseSimulator.HorizontalScroll(x);
             //_mouseSimulator.VerticalScroll(y);
         }
+        //////////////////////////////////
         private void Button_A(State state)
         {
             var isAdown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.A);
@@ -86,7 +95,27 @@ namespace Xbox360AsMouse
             var isBackdown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.Back);
             if (isBackdown) Console.WriteLine("Back: {0}", isBackdown);
         }
-
+        //////////////////////////////////
+        private void Button_DPadDown(State state)
+        {
+            var isDPadDowndown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadDown);
+            if (isDPadDowndown) Console.WriteLine("DPadDown: {0}", isDPadDowndown);
+        }
+        private void Button_DPadUp(State state)
+        {
+            var isDPadUpdown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadUp);
+            if (isDPadUpdown) Console.WriteLine("DPadUp: {0}", isDPadUpdown);
+        }
+        private void Button_DPadLeft(State state)
+        {
+            var isDPadLeftdown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadLeft);
+            if (isDPadLeftdown) Console.WriteLine("DPadLeft: {0}", isDPadLeftdown);
+        }
+        private void Button_DPadRight(State state)
+        {
+            var isDPadRightdown = state.Gamepad.Buttons.HasFlag(GamepadButtonFlags.DPadRight);
+            if (isDPadRightdown) Console.WriteLine("DPadRight: {0}", isDPadRightdown);
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
